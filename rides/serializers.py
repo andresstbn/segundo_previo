7d02b8d9e3851ea70f13a10146cb622e167391fa
DIEGUS15 <diegogranados1501@gmail.com>
2025-05-09 07:37:04 -0500

    ajustes realizados para el cumplimiento de los retos propuestos

--- rides/serializers.py
+++ rides/serializers.py
@@ -6,6 +6,8 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    average_score = serializers.FloatField(read_only=True, required=False)
+    
     class Meta:
         model = User
         fields = [
@@ -17,6 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
             'is_driver',
             'is_passenger',
             'is_available',
+            'average_score',
         ]
 
 
@@ -50,6 +53,7 @@ class TripSerializer(serializers.ModelSerializer):
     passenger = UserSerializer(read_only=True)
     driver = UserSerializer(read_only=True)
     rating = RatingSerializer(read_only=True)
+    fare = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Trip
@@ -62,8 +66,24 @@ class TripSerializer(serializers.ModelSerializer):
             'end_time',
             'status',
             'rating',
+            'fare',
         ]
 
     def create(self, validated_data):
-        # Passthrough to viewset logic; trip.request view will assign driver
         return super().create(validated_data)
+    
+    def get_fare(self, obj):
+        base_fare = 1000
+        
+        if obj.driver:
+            active_trips_of_driver = Trip.objects.filter(
+                driver=obj.driver, 
+                status__in=[Trip.STATUS_PENDING, Trip.STATUS_ONGOING]
+            ).count()
+            
+            surge_multiplier = 1 + (active_trips_of_driver / 10)
+            fare = int(base_fare * surge_multiplier)
+        else:
+            fare = base_fare
+            
+        return fare
