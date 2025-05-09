6bbe98b8bf806b180c1e94900b9e6511dc5358a1
JosuePerez4 <josuedanielpg@ufps.edu.co>
2025-05-09 07:42:26 -0500

    Mi intento de parcial

--- rides/views.py
+++ rides/views.py
@@ -2,6 +2,11 @@ from django.contrib.auth import get_user_model
 from django.views.generic import TemplateView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import status
+from django.db.models import Avg
 
 from .models import Rating, Trip, Vehicle
 from .serializers import (
@@ -25,6 +30,15 @@ class VehicleViewSet(viewsets.ModelViewSet):
     """
     ViewSet para CRUD de vehículos.
     """
+    
+    @action(detail=False, methods=['get'], url_path='models')
+    def models(self, request):
+        vehicles = Vehicle.objects.all()
+        models = vehicles.values_list('model', flat=True).distinct()
+        modelsCounter = vehicles.values('model').annotate(count=Count('model'))
+        return Response(modelsCounter)
+        
+    
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -47,6 +61,16 @@ class DriverViewSet(viewsets.ReadOnlyModelViewSet):
     """
     ViewSet para ver conductores.
     """
+    
+    @action(detail=True, methods=['get'], url_path='trending')
+    def get_ratings(self, request, pk=None):
+        drivers = User.objects.filter(is_driver=True).annotate(
+            average_score=Avg('trips_as_driver__rating')
+        )
+        
+        serializer = UserSerializer(drivers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
     queryset = User.objects.filter(is_driver=True)
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -63,4 +87,17 @@ class RatingViewSet(
     """
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class passengerViewSet(viewsets.ReadOnlyModelViewSet):
+    
+    @action(detail=True, methods=['get'], url_path='trips')
+    def get_passenger_trips(self, request, pk=None):
+        trips = Trip.objects.filter(passenger=pk)
+        serializer = TripSerializer(trips, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    
+    queryset = User.objects.filter(is_passenger=True)
+    serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
